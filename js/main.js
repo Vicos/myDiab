30d4315cd5a0283dfb6fc606a4227bb543bb4b0a
Vicos <vicos51@gmail.com>
2013-07-27 02:34:13 +0200

    Add require.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-
+require.config({
+    paths: {
+        jquery: 'vendor/jquery-1.10.1.min',
+        bootstrap: 'vendor/bootstrap.min',
+    },
+    shim: {
+        "bootstrap": {
+            deps: ["jquery"]
+        }
+    }
+});
