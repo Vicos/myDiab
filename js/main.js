6097bb29471e43de8a488baf96725b900142d18c
Vicos <vicos51@gmail.com>
2013-07-28 12:57:52 +0200

    Use development version of Backbone

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ require.config({
         text: 'vendor/require.text',
         jquery: 'vendor/jquery-1.10.1.min',
         underscore: 'vendor/underscore-min',
-        backbone: 'vendor/backbone-min',
+        backbone: 'vendor/backbone',
         bootstrap: 'vendor/bootstrap.min'
     },
     shim: {
